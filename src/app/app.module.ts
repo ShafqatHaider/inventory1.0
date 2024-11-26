423736ad7947945db1b7330a206800281c104bae
Shafqat Haider <93528873+ShafqatHaider@users.noreply.github.com>
2024-11-26 15:13:20 +0500

    m

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { SidebarComponent } from './layout/sidebar/sidebar.component';
+import { DashboardComponent } from './layout/dashboard/dashboard.component';
+import { CardComponent } from './shared/card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent,
+    DashboardComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
