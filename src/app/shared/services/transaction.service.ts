13dd906be86945e19541541576bd8ebb7af40260
Shafqat Haider <93528873+ShafqatHaider@users.noreply.github.com>
2024-11-27 18:12:39 +0500

    calculatemethods

--- src/app/shared/services/transaction.service.ts
+++ src/app/shared/services/transaction.service.ts
@@ -6,4 +6,19 @@ import { Injectable } from '@angular/core';
 export class TransactionService {
 
   constructor() { }
+
+
+
+  calculateQty(obj:any){
+    let qty=obj.pktQty*obj.qty;
+    return qty;
+  }
+  calculateAmount(obj:any){
+    let amt=0
+    for(let i=0; i<obj.length; i++){
+      amt=amt+obj[i].amount;
+    }
+    return amt
+  }
+
 }
