e6d9f94ee1d0412c021ec2f825f89490d8e76cb4
Shafqat Haider <93528873+ShafqatHaider@users.noreply.github.com>
2024-11-27 18:15:58 +0500

    please do it

--- src/app/shared/services/transaction.service.ts
+++ src/app/shared/services/transaction.service.ts
@@ -11,12 +11,16 @@ export class TransactionService {
 
   calculateQty(obj:any){
     let qty=obj.pktQty*obj.qty;
+    this.calculateSubAmount(qty,obj.amount)
     return qty;
+  }
+  calculateSubAmount(obj:any, amount:any){
+    return amount*obj;
   }
   calculateAmount(obj:any){
     let amt=0
     for(let i=0; i<obj.length; i++){
-      amt=amt+obj[i].amount;
+      amt=obj[i].amount*(obj[i].pktQty*obj[i].packet);
     }
     return amt
   }
