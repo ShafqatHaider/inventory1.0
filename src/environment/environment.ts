3b393189dbf0b3e61987f1dce3f962dacb0736ef
Shafqat Haider <93528873+ShafqatHaider@users.noreply.github.com>
2024-11-29 16:11:43 +0500

    new commit

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,9 +1,9 @@
 export const environment={
     branchId:4,//Number(localStorage.getItem('BRANCH_ID')),
     companyId:4,//Number(localStorage.getItem('COMPANY_ID')),
-    operatorID:0, //Number(localStorage.getItem('OPERATOR_ID'));
+    operatorID:0,//Number(localStorage.getItem('COMPANY_ID')),
     production: false,
-     // baseApiUrl: 'http://localhost:5000/api/',
-        baseApiUrl: 'https://di.easwdb.com/api/', //main,
+     baseApiUrl: 'http://localhost:5000/api/',
+        // baseApiUrl: 'https://di.easwdb.com/api/', //main,
         
 }
