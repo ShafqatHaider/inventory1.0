a0ca3e8bd3d73ee6ff327d45035c6695a7a320a5
Shafqat Haider <93528873+ShafqatHaider@users.noreply.github.com>
2024-11-29 11:29:05 +0500

    first commit of the day

--- src/app/accounts/components/rbookindex/rbookindex.component.ts
+++ src/app/accounts/components/rbookindex/rbookindex.component.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AccountConfigurations } from '../../supportives/AccountConfigurations';
+import { Observables } from '../../../shared/services/observers';
 
 @Component({
   selector: 'app-rbookindex',
   templateUrl: './rbookindex.component.html',
   styleUrl: './rbookindex.component.scss'
 })
-export class RbookindexComponent {
+export class RbookindexComponent implements OnInit{
 add(){}
 edit(){}
 delete(){}
 config=AccountConfigurations.receipt
+/**
+ *
+ */
+constructor(private observer:Observables) {
+  
+  
+}
+ngOnInit(): void {
+  
+}
+getData(){
+this.observer.getLookups(this.config.endPoints.getAll).subscribe(res=>this.config.data=res)
+}
 }
--- src/app/inventory/code-index/code-index.component.ts
+++ src/app/inventory/code-index/code-index.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { InventoryConfigurations } from '../supportive/InventoryConfigurations';
 
 @Component({
   selector: 'app-code-index',
   templateUrl: './code-index.component.html',
   styleUrl: './code-index.component.scss'
 })
-export class CodeIndexComponent {
+export class CodeIndexComponent implements OnInit{
+config= InventoryConfigurations.code;
 
+ngOnInit(): void {
+  this.config.data
+}
+add(){}
+edit(){}
+delete(){}
 }
